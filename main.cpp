431b2731e5ff834b0cc398737177e773a48b72e9
Lolit Bairiganjan <wolfrainegaming.you@gmail.com>
2026-08-25 21:43:19 +0530

    Made a simple usable version of the KawaiiCalc

--- main.cpp
+++ main.cpp
@@ -1,15 +1,27 @@
 #include <QGuiApplication>
 #include <QQmlApplicationEngine>
-
+#include <QQmlContext>
+#include "calcengine.h"
 
 int main(int argc, char *argv[])
 {
+    qputenv("QSG_RENDER_LOOP", "basic");
+
     QGuiApplication app(argc, argv);
 
+    CalcEngine calcEngine;
     QQmlApplicationEngine engine;
-    QObject::connect(&engine, &QQmlApplicationEngine::objectCreationFailed,
-                     &app, []() { QCoreApplication::exit(-1); },
-    Qt::QueuedConnection);
+
+    engine.rootContext()->setContextProperty("calc", &calcEngine);
+
+    QObject::connect(
+        &engine,
+        &QQmlApplicationEngine::objectCreationFailed,
+        &app,
+        []() { QCoreApplication::exit(-1); },
+        Qt::QueuedConnection
+    );
+
     engine.loadFromModule("KawaiiCalc", "Main");
 
     return app.exec();
